d723337c6516a0beb806dfbe299971c219acdf6d
Adolfo <allorenter@gmail.com>
2019-11-06 14:00:01 +0100

    Instalación multer y funcionalidades avatar usuarios

--- src/controllers/controllerUsers.js
+++ src/controllers/controllerUsers.js
@@ -4,6 +4,8 @@ import { tablaUsuarios } from "../utils/BBDD";
 import Usuario from "../utils/Usuario";
 import Respuesta from "../utils/Respuestas";
 import jwt from "../utils/JWT";
+import multer from "multer";
+import fs from "fs";
 
 const controllerUsers = {
   login: function(nombre, contrasena, callback) {
@@ -11,19 +13,28 @@ const controllerUsers = {
     tablaUsuarios
       .login(usuario)
       .then(result => {
-        if (result.length > 0) {
-          return callback(
-            Respuesta.success({
-              mensaje: "Login correcto",
-              usuario: usuario.nombre,
-              token: jwt.generarToken(usuario)
-            })
-          );
-        } else {
-          return callback(
-            Respuesta.error(401, "Usuario o contraseña incorrectos")
-          );
-        }
+        if (result.length === 0) return callback(Respuesta.error(401, "Usuario o contraseña incorrectos"));
+          fs.readFile("uploads/avatar/"+nombre+".png", (err, data)=>{
+            //Si hay un error en la lectura del fichero, asognamos null al avatar
+            if(err) return callback(
+              Respuesta.success({
+                mensaje: "Login correcto",
+                usuario: usuario.nombre,
+                token: jwt.generarToken(usuario),
+                avatar: null
+              })
+            );
+            let base64Image = new Buffer.from(data, 'binary').toString('base64');
+            imgSrcString = `data:image/png;base64,${base64Image}`;
+            return callback(
+              Respuesta.success({
+                mensaje: "Login correcto",
+                usuario: usuario.nombre,
+                token: jwt.generarToken(usuario),
+                avatar: imgSrcString
+              })
+            );
+          })
       })
       .catch(e => {
         return callback(Respuesta.error(404, "Ha ocurrido un error"));
@@ -87,18 +98,58 @@ const controllerUsers = {
   getConfigHome: function(token, callback) {
     tablaUsuarios
       .getConfigHome(jwt.verificarToken(token).nombre)
-      .then((result)=>{
-        if(result.length>0){
-          return callback(Respuesta.success({configHome: JSON.parse(result[0].config)}));
-        }else{
+      .then(result => {
+        if (result.length > 0) {
+          return callback(Respuesta.success({ configHome: JSON.parse(result[0].config) }));
+        } else {
           return callback(Respuesta.error(401, "Token inválido"));
         }
       })
-      .catch((e)=>{
+      .catch(e => {
         return callback(Respuesta.error(404, "Ha ocurrido un error"));
       });
+  },
+
+  subirAvatar: function(req, res, callback) {
+    var uploadFnct = function(nombreUsuario){
+      var storage = multer.diskStorage({ //multers disk storage settings
+          destination: function (req, file, cb) {
+              cb(null, 'uploads/avatar');
+          },
+          filename: function (req, file, cb) {
+            cb(null, nombreUsuario+".png");
+          }
+      });
+      var upload = multer({ //multer settings
+                      storage: storage
+                  }).single('avatar');
+    
+      return upload;
+    };
+    var currUpload = uploadFnct(jwt.verificarToken(req.headers["authorization"]).nombre);
+    currUpload(req,res,function(err){
+      if(err){
+        return callback(Respuesta.error(404, "Ha ocurrido un error"));
+      }
+      return callback(Respuesta.success({image: req.file}));
+    });
+  },
+
+  getAvatar: function(token, callback){
+    let nombreUsuario=jwt.verificarToken(token).nombre;
+    
+    fs.readFile("uploads/avatar/"+nombreUsuario+".png", (err, data)=>{
       
+      if(err) return callback(Respuesta.error(404, "Ha ocurrido un error"));
+      
+      let base64Image = new Buffer.from(data, 'binary').toString('base64');
+      
+      let imgSrcString = `data:image/png;base64,${base64Image}`;
+      
+      return callback(Respuesta.success({image: imgSrcString}));
+  })
   }
+
 };
 
 export default controllerUsers;
--- src/routes/routesUsers.js
+++ src/routes/routesUsers.js
@@ -5,32 +5,53 @@ import Respuesta from "../utils/Respuestas";
 
 var router = express.Router();
 
-router.post('/login', function(req, res, next) {
-  controllerUsers.login(req.body.nombreUsuario, req.body.contrasena, function(result) {
+router.post("/login", function(req, res, next) {
+  controllerUsers.login(req.body.nombreUsuario, req.body.contrasena, function(
+    result
+  ) {
     Respuesta.enviarRespuesta(result, res);
   });
 });
 
-router.post('/crearUsuario', function(req, res, next) {
-  controllerUsers.crearUsuario(req.body.nombreUsuario, req.body.email, req.body.contrasena, function(result) {
-    Respuesta.enviarRespuesta(result, res);
-  });
+router.post("/crearUsuario", function(req, res, next) {
+  controllerUsers.crearUsuario(
+    req.body.nombreUsuario,
+    req.body.email,
+    req.body.contrasena,
+    function(result) {
+      Respuesta.enviarRespuesta(result, res);
+    }
+  );
 });
 
-router.get('/usuarioExiste', function(req, res, next) {
+router.get("/usuarioExiste", function(req, res, next) {
   controllerUsers.usuarioExiste(req.query.nombreUsuario, function(result) {
     Respuesta.enviarRespuesta(result, res);
   });
 });
 
-router.get('/comprobarToken', function(req, res, next) {
-  controllerUsers.comprobarToken(req.headers['authorization'], function(result) {
+router.get("/comprobarToken", function(req, res, next) {
+  controllerUsers.comprobarToken(req.headers["authorization"], function(result) {
     Respuesta.enviarRespuesta(result, res);
   });
 });
 
-router.get('/getConfigHome', function(req, res, next) {
-  controllerUsers.getConfigHome(req.headers['authorization'], function(result) {
+router.get("/getConfigHome", function(req, res, next) {
+  console.log(req.headers["authorization"]);
+  
+  controllerUsers.getConfigHome(req.headers["authorization"], function(result) {
+    Respuesta.enviarRespuesta(result, res);
+  });
+});
+
+router.post("/subirAvatar", function(req, res, next) {
+  controllerUsers.subirAvatar(req, res, function(result) {
+    Respuesta.enviarRespuesta(result, res);
+  });
+});
+
+router.get("/getAvatar", function(req, res, next) {
+  controllerUsers.getAvatar(req.headers["authorization"], function(result) {
     Respuesta.enviarRespuesta(result, res);
   });
 });
